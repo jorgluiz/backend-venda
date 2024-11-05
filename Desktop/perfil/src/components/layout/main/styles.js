8599f30081793086c4bed3541df59a2d6647c080
jorgluiz <jorgeluizti@hotmail.com>
2024-11-05 17:32:17 -0300

    refactor: add route in  pages/projetos/stripe

---
+++ Desktop/perfil/src/components/layout/main/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+height: 100%;
+width: 100%;
+background-color: #E4E7EB;
+
+/* @media (max-width: 990px) {
+width: 100%;
+margin-left: 0px;
+} */
+`;
---
+++ Desktop/perfil/src/pages/projetos/carousel/index.jsx
@@ -0,0 +1,16 @@
+import Main from "@/components/layout/main";
+import SideBar from "@/components/layout/sideBar";
+import Link from 'next/link';
+
+const carousel = () => {
+  return (
+    <>
+      <SideBar></SideBar>
+      <Main>
+
+      </Main>
+    </>
+  );
+};
+
+export default carousel;
